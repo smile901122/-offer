433a35e7bebfeab2c5241a64bd18716683921a5a
momosmile <443106364@qq.com>
2016-08-09 23:11:48 +0800

    Create 61.按之字形顺序打印二叉树.cpp

---
+++ 61.按之字形顺序打印二叉树.cpp
@@ -0,0 +1,37 @@
+/*
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};
+*/
+class Solution {
+public:
+    vector<vector<int> > Print(TreeNode* pRoot) {
+        vector<vector<int> > res;
+        if(pRoot == NULL)	return res;
+        deque<TreeNode*> q;
+        q.push_back(pRoot);
+        bool flag = false;
+        while(!q.empty()){
+            int n = q.size();
+            vector<int> level;
+            while(n--){
+                TreeNode *p = q.front();
+                q.pop_front();
+                level.push_back(p->val);
+                if(p->left)		q.push_back(p->left);
+                if(p->right)	q.push_back(p->right);
+            }
+            
+            if(flag)	reverse(level.begin(), level.end());
+            flag = !flag;
+            res.push_back(level);
+        }
+        return res;
+    }
+    
+};
