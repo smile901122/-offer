b9f8bfaf3965e7e147b4b3049afb436141f0adec
momosmile <443106364@qq.com>
2016-08-10 00:37:00 +0800

    Create 25.二叉树中和为某一值的路径.cpp

---
+++ 25.二叉树中和为某一值的路径.cpp
@@ -0,0 +1,33 @@
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    vector<vector<int> > FindPath(TreeNode* root,int expectNumber) {
+		vector<vector<int> > res;
+        if(root == NULL)	return res;
+        vector<int> path;
+        int sum = 0;
+        find(root, res, path, sum, expectNumber);
+        return res;
+    }
+private:
+    void find(TreeNode *root, vector<vector<int> > &res, vector<int> &path, int sum, const int expectNumber){
+        path.push_back(root->val);
+        sum += root->val;
+        
+        bool isLeaf = (root->left == NULL) && (root->right == NULL);
+        if(isLeaf && sum == expectNumber)	res.push_back(path);
+        
+        if(root->left)	find(root->left, res, path, sum, expectNumber);
+        if(root->right)	find(root->right, res, path, sum, expectNumber);
+        
+        path.pop_back();
+    }
+};
